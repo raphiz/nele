1a1420753640dc0d9b192313753f3b0de1813c25
Raphael Zimmermann <mail@raphael.li>
2016-08-20 22:33:22 +0200

    Back to snapshot

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name="nele",
-    version="0.1.0",
+    version="0.1.0.dev",
     packages=['nele'],
     author="Raphael Zimmermann",
     author_email="dev@raphael.li",
