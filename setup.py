898a2401ae566325464c98b0746b4de569de04f2
Raphael Zimmermann <mail@raphael.li>
2017-04-14 16:05:59 +0200

    Bump version: 0.2.0 → 0.3.0.dev

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name="nele",
-    version="0.2.0",
+    version="0.3.0.dev",
     packages=['nele'],
     author="Raphael Zimmermann",
     author_email="dev@raphael.li",
