b3f8d793163124eb0ab96e024f99926653c41142
Raphael Zimmermann <mail@raphael.li>
2020-03-09 07:52:09 +0100

    Remove beta classifier

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
-        "Programming Language :: Python :: Implementation :: CPython",
-        'Development Status :: 4 - Beta',
+        "Programming Language :: Python :: Implementation :: CPython"
     ],
 )
